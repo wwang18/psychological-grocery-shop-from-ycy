da57855325f3c2c88d04ee7f8c105e2eeeb91bb0
WeiWang88 <wwang18@kent.edu>
2019-03-25 02:00:03 -0500

    - update send mail page

--- psychological_grocery_shop_from_ycy/screens/PageMailToChaoYue_j.js
+++ psychological_grocery_shop_from_ycy/screens/PageMailToChaoYue_j.js
@@ -16,12 +16,29 @@ class PageMailToChaoYue_j extends Component {
     this.props.navigation.goBack();
   }
 
-   _onPressButton_mail_box() {
-     Alert.alert("点击邮箱");
+   _onPressButton_family() {
+     Alert.alert("家庭");
+     this.props.navigation.push("SubTopic")
    }
 
-   _onPressButton_mail_to_ChaoYue() {
-     this.props.navigation.push("MailBox_i")
+   _onPressButton_work() {
+     Alert.alert("工作");
+     this.props.navigation.push("SubTopic")
+   }
+
+   _onPressButton_love() {
+     Alert.alert("爱情");
+     this.props.navigation.push("SubTopic")
+   }
+
+   _onPressButton_social() {
+     Alert.alert("人际");
+     this.props.navigation.push("SubTopic")
+   }
+
+   _onPressButton_Chaoyue() {
+     Alert.alert("超越");
+     this.props.navigation.push("SubTopic")
    }
 
   render() {
@@ -30,47 +47,47 @@ class PageMailToChaoYue_j extends Component {
         style={styles.container}
         source={require("./../img/j_page.gif")}
       >
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{flex: 1, flexDirection: 'column'}}>
                     <View style={{flex:80}}/>
                     <View style={{flex:132}}/>
                     <View style={{flex:71}}/>
                     <View style={{flex:110}}>
-                      <View style={{flex: 1, flexDirection: 'column'}}>
+                      <View style={{flex: 1, flexDirection: 'row'}}>
                                     <View style={{flex:316}}/>
-                                    <View style={{flex:260}}>
-                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_box()}/>
+                                    <View style={{flex:260, backgroundColor: 'powderblue'}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_family()}/>
                                     </View>
                                     <View style={{flex:182}}/>
-                                    <View style={{flex:260}}>
-                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_box()}/>
+                                    <View style={{flex:260, backgroundColor: 'powderblue'}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_work()}/>
                                     </View>
                                     <View style={{flex:316}}/>
                       </View>
                     </View>
                     <View style={{flex:30}}/>
                     <View style={{flex:110}}>
-                      <View style={{flex: 1, flexDirection: 'column'}}>
+                      <View style={{flex: 1, flexDirection: 'row'}}>
                                     <View style={{flex:316}}/>
-                                    <View style={{flex:260}}>
-                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_box()}/>
+                                    <View style={{flex:260, backgroundColor: 'powderblue'}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_love()}/>
                                     </View>
                                     <View style={{flex:182}}/>
-                                    <View style={{flex:260}}>
-                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_box()}/>
+                                    <View style={{flex:260, backgroundColor: 'powderblue'}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_social()}/>
                                     </View>
                                     <View style={{flex:316}}/>
                       </View>
                     </View>
                     <View style={{flex:60}}/>
                     <View style={{flex:110}}>
-                      <View style={{flex: 1, flexDirection: 'column'}}>
+                      <View style={{flex: 1, flexDirection: 'row'}}>
                                     <View style={{flex:378}}/>
-                                    <View style={{flex:578}}>
-                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_box()}/>
+                                    <View style={{flex:578, backgroundColor: 'powderblue'}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_Chaoyue()}/>
                                     </View>
                                     <View style={{flex:378}}/>
                       </View>
-                    </view>
+                    </View>
                     <View style={{flex:47}}/>
       </View>
       </ImageBackground>
