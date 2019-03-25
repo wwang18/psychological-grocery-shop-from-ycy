e476a219a37658e323dcb70a401d4c4a9d2ddf03
WeiWang88 <wwang18@kent.edu>
2019-03-24 23:37:42 -0500

    - add page mailbox

--- psychological_grocery_shop_from_ycy/screens/PageMailBox_i.js
+++ psychological_grocery_shop_from_ycy/screens/PageMailBox_i.js
@@ -16,12 +16,53 @@ class PageMailBox_i extends Component {
     this.props.navigation.goBack();
   }
 
+   _onPressButton_mail_box() {
+     Alert.alert("点击邮箱");
+   }
+
+   _onPressButton_mail_to_ChaoYue() {
+     Alert.alert("写信给超越");
+   }
+
   render() {
     return (
       <ImageBackground
         style={styles.container}
         source={require("./../img/i_page.gif")}
       >
+      <View style={{flex: 1, flexDirection: 'row'}}>
+              <View style={{flex:184}}/>
+              <View style={{flex:218}}>
+                      <View style={{flex: 1, flexDirection: 'column'}}>
+                                    <View style={{flex:605}}/>
+                                    <View style={{flex:107}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_box()}/>
+                                    </View>
+                                    <View style={{flex:38}}/>
+                      </View>
+              </View>
+              <View style={{flex:300}}/>
+              <View style={{flex:455}}>
+                    <View style={{flex: 1, flexDirection: 'column'}}>
+                                    <View style={{flex:605}}/>
+                                    <View style={{flex:107}}>
+                                        <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_mail_to_ChaoYue()}/>
+                                    </View>
+                                    <View style={{flex:38}}/>
+                    </View>
+              </View>
+              <View style={{flex:57}}/>
+              <View style={{flex:86}}>
+                    <View style={{flex: 1, flexDirection: 'column'}}>
+                                                        <View style={{flex:690}}/>
+                                                        <View style={{flex:43}}>
+                                                            <TouchableOpacity style={{flex: 1}} onPress={() => this._onPressButton_back()}/>
+                                                        </View>
+                                                        <View style={{flex:17}}/>
+                    </View>
+              </View>
+              <View style={{flex:34}}/>
+      </View>
       </ImageBackground>
     );
   }
