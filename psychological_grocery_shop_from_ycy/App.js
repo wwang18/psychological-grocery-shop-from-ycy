c7182eccf4c2bcf43ab6af4655739f1c6c3be307
WeiWang88 <wwang18@kent.edu>
2019-03-27 18:07:20 -0500

    - prepare to add 5 new pages. N, P, P1, Q, Q1

--- psychological_grocery_shop_from_ycy/App.js
+++ psychological_grocery_shop_from_ycy/App.js
@@ -5,8 +5,13 @@ import Home from "./screens/Home";
 import PageWriteLetter from "./screens/PageWriteLetter";
 import SubTopic from "./screens/SubTopic";
 import LetterPaper from "./screens/LetterPaper";
-import PageMailBox_i from './screens/PageMailBox_i';
-import PageMailToChaoYue_j from './screens/PageMailToChaoYue_j';
+import PageMailBox_i from "./screens/PageMailBox_i";
+import PageMailToChaoYue_j from "./screens/PageMailToChaoYue_j";
+import PageGiftCardView_q from "./screens/PageGiftCardView_q";
+import PageGiftCard_q from "./screens/PageGiftCard_q";
+import PageMailBox_n from "./screens/PageMailBox_n";
+import PagePersonCard_p from "./screens/PagePersonCard_p";
+import PagePersonCardView_p from "./screens/PagePersonCardView_p";
 
 const AppNavigator = createStackNavigator(
   {
@@ -34,18 +39,48 @@ const AppNavigator = createStackNavigator(
         header: null
       }
     },
-	MailBox_i: {
-       screen: PageMailBox_i,
-       navigationOptions: {
+    MailBox_i: {
+      screen: PageMailBox_i,
+      navigationOptions: {
         header: null
-       }
-      },
-	MailToChaoYue_j: {
-       screen: PageMailToChaoYue_j,
-       navigationOptions: {
+      }
+    },
+    MailToChaoYue_j: {
+      screen: PageMailToChaoYue_j,
+      navigationOptions: {
         header: null
-       }
-      },
+      }
+    },
+    PageGiftCardView_q: {
+      screen: PageGiftCardView_q,
+      navigationOptions: {
+        header: null
+      }
+    },
+    PageGiftCard_q: {
+      screen: PageGiftCard_q,
+      navigationOptions: {
+        header: null
+      }
+    },
+    PageMailBox_n: {
+      screen: PageMailBox_n,
+      navigationOptions: {
+        header: null
+      }
+    },
+    PagePersonCard_p: {
+      screen: PagePersonCard_p,
+      navigationOptions: {
+        header: null
+      }
+    },
+    PagePersonCardView_p: {
+      screen: PagePersonCardView_p,
+      navigationOptions: {
+        header: null
+      }
+    }
   },
   {
     initialRouteName: "Home"
