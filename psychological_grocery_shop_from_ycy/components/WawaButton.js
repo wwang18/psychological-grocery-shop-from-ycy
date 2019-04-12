f28c2363fbb0bb2c63ae2f54ff530ddf1998fa61
陈健 <740103035@qq.com>
2019-04-12 16:09:15 +0800

    ios 样式适配 新增公共样式表，机型适配

--- psychological_grocery_shop_from_ycy/components/WawaButton.js
+++ psychological_grocery_shop_from_ycy/components/WawaButton.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 import { scale } from 'react-native-size-matters';
 
-import WawaText from './WawaText'
+import WawaText from './WawaText';
+import cSty from '../utils/commonStyle'
 
 function getBackGroundImage(size) {
   if (size === 'sm' || size === 'small') {
@@ -26,7 +27,7 @@ export const WawaButton = (props) => {
       style={props.style}>
       <ImageBackground
         resizeMode="stretch"
-        style={styles.buttonBackgournd}
+        style={[styles.buttonBackgournd, cSty.cc]}
         source={getBackGroundImage(props.size)}>
         <WawaText style={styles.buttonText}>{props.text}</WawaText>
       </ImageBackground>
@@ -36,12 +37,10 @@ export const WawaButton = (props) => {
 
 const styles = StyleSheet.create({
   buttonText: {
-    width: '100%',
-    height: '100%',
     color: 'white',
     fontSize: scale(16),
     textAlign: 'center',
-    textAlignVertical: 'center',
+    textAlignVertical: 'center'
   },
   buttonBackgournd: {
     width: '100%',
--- psychological_grocery_shop_from_ycy/index.js
+++ psychological_grocery_shop_from_ycy/index.js
@@ -7,4 +7,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+console.ignoredYellowBox = ['Warning: BackAndroid is deprecated. Please use BackHandler instead.','source.uri should not be an empty string','Invalid props.style key'];
+console.disableYellowBox = true // 关闭全部黄色警告
 AppRegistry.registerComponent(appName, () => App);
