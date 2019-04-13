da5a754a4a95257555d6761ce8614569f9768e21
陈健 <740103035@qq.com>
2019-04-14 01:06:50 +0800

    新增react-native-storage库 用于存储本地数据

--- psychological_grocery_shop_from_ycy/App.js
+++ psychological_grocery_shop_from_ycy/App.js
@@ -20,9 +20,28 @@ import PageStoredLetter_o4 from "./screens/PageStoredLetter_o4";
 import instoreRoutes from './screens/InStore/InStoreRouter';
 import PageTeamIntro from "./screens/PageTeamIntro";
 
+import Storage from 'react-native-storage';
+import { AsyncStorage } from 'react-native';
 import dva from './utils/dva';
 import NavigationService from "./utils/navigationService";
 import mailbox from './models/mailBox'; // 邮箱模块数据
+
+const storage = new Storage({
+  // 最大容量，默认值1000条数据循环存储
+  size: 5000,
+  // 存储引擎：对于RN使用AsyncStorage，对于web使用window.localStorage
+  // 如果不指定则数据只会保存在内存中，重启后即丢失
+  storageBackend: AsyncStorage,
+    
+  // 数据过期时间，默认一整天（1000 * 3600 * 24 毫秒），设为null则永不过期
+  defaultExpires: 1000 * 3600 * 24 * 31,
+    
+  // 读写时在内存中缓存数据。默认启用。
+  enableCache: true,
+  // sync: require('你可以另外写一个文件专门处理sync')  
+  	
+})  
+global.Storage = storage;
 
 const AppNavigator = createStackNavigator(
   {
