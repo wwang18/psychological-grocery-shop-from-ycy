a3e3f663956b86b5c8164f6abac74d658ff475e9
WeiWang88 <wwang18@kent.edu>
2019-04-14 00:21:26 -0500

    - add all cards to the card store

--- psychological_grocery_shop_from_ycy/components/CardStore/giftConfig.js
+++ psychological_grocery_shop_from_ycy/components/CardStore/giftConfig.js
@@ -1,23 +1,114 @@
 export default [
-    {
-        id: 0,
-        small: require("../../img/giftcards/giftCardsSmall/Image01.webp"),
-        big: require('../../img/giftcards/giftCards/Image01.webp'),
-        unlocked: true,
-        key: []
-    },
-    {
-        id: 1,
-        small: require("../../img/giftcards/giftCardsSmall/Image02.webp"),
-        big: require('../../img/giftcards/giftCards/Image01.webp'),
-        unlocked: true,
-        key: []
-    },
-    {
-        id: 2,
-        small: require("../../img/giftcards/giftCardsSmall/Image03.webp"),
-        big: require('../../img/giftcards/giftCards/Image01.webp'),
-        unlocked: false,
-        key: []
-    },
+  {
+    id: 0,
+    small: require("../../img/giftcards/giftCardsSmall/Image01.webp"),
+    big: require("../../img/giftcards/giftCards/Image01.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 1,
+    small: require("../../img/giftcards/giftCardsSmall/Image02.webp"),
+    big: require("../../img/giftcards/giftCards/Image02.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 2,
+    small: require("../../img/giftcards/giftCardsSmall/Image03.webp"),
+    big: require("../../img/giftcards/giftCards/Image03.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 3,
+    small: require("../../img/giftcards/giftCardsSmall/Image04.webp"),
+    big: require("../../img/giftcards/giftCards/Image04.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 4,
+    small: require("../../img/giftcards/giftCardsSmall/Image05.webp"),
+    big: require("../../img/giftcards/giftCards/Image05.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 5,
+    small: require("../../img/giftcards/giftCardsSmall/Image06.webp"),
+    big: require("../../img/giftcards/giftCards/Image06.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 6,
+    small: require("../../img/giftcards/giftCardsSmall/Image07.webp"),
+    big: require("../../img/giftcards/giftCards/Image07.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 7,
+    small: require("../../img/giftcards/giftCardsSmall/Image08.webp"),
+    big: require("../../img/giftcards/giftCards/Image08.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 8,
+    small: require("../../img/giftcards/giftCardsSmall/Image09.webp"),
+    big: require("../../img/giftcards/giftCards/Image09.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 9,
+    small: require("../../img/giftcards/giftCardsSmall/Image10.webp"),
+    big: require("../../img/giftcards/giftCards/Image10.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 10,
+    small: require("../../img/giftcards/giftCardsSmall/Image11.webp"),
+    big: require("../../img/giftcards/giftCards/Image11.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 11,
+    small: require("../../img/giftcards/giftCardsSmall/Image12.webp"),
+    big: require("../../img/giftcards/giftCards/Image12.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 12,
+    small: require("../../img/giftcards/giftCardsSmall/Image13.webp"),
+    big: require("../../img/giftcards/giftCards/Image13.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 13,
+    small: require("../../img/giftcards/giftCardsSmall/Image14.webp"),
+    big: require("../../img/giftcards/giftCards/Image14.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 14,
+    small: require("../../img/giftcards/giftCardsSmall/Image15.webp"),
+    big: require("../../img/giftcards/giftCards/Image15.webp"),
+    unlocked: true,
+    key: []
+  },
+  {
+    id: 15,
+    small: require("../../img/giftcards/giftCardsSmall/Image16.webp"),
+    big: require("../../img/giftcards/giftCards/Image16.webp"),
+    unlocked: true,
+    key: []
+  }
 ];
