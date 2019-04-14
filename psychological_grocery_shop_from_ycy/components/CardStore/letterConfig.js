246632c3c65cc5f9c51d9f514b15613bd7a0519c
WeiWang88 <wwang18@kent.edu>
2019-04-13 23:22:43 -0500

    - minor modification to make the app runnable

--- psychological_grocery_shop_from_ycy/components/CardStore/letterConfig.js
+++ psychological_grocery_shop_from_ycy/components/CardStore/letterConfig.js
@@ -1,25 +1,25 @@
 export default [
     {
         id: 0,
-        small: require("../../img/O/returnedMailCard_small/Image03.webp"),
-        big: require('../../img/O/returnedMailCard/Image03.webp'),
-        back: require('../../img/O/returnedMailCard/Image02.webp'),
+        small: require("../../img/returnedMailCards/returnedMailCard_small/Image03.webp"),
+        big: require('../../img/returnedMailCards/returnedMailCard/Image03.webp'),
+        back: require('../../img/returnedMailCards/returnedMailCard/Image02.webp'),
         unlocked: true,
         key: []
     },
     {
         id: 1,
-        small: require("../../img/O/returnedMailCard_small/Image05.webp"),
-        big: require('../../img/O/returnedMailCard/Image05.webp'),
-        back: require('../../img/O/returnedMailCard/Image04.webp'),
+        small: require("../../img/returnedMailCards/returnedMailCard_small/Image05.webp"),
+        big: require('../../img/returnedMailCards/returnedMailCard/Image05.webp'),
+        back: require('../../img/returnedMailCards/returnedMailCard/Image04.webp'),
         unlocked: true,
         key: []
     },
     {
         id: 2,
-        small: require("../../img/O/returnedMailCard_small/Image07.webp"),
-        big: require('../../img/O/returnedMailCard/Image07.webp'),
-        back: require('../../img/O/returnedMailCard/Image06.webp'),
+        small: require("../../img/returnedMailCards/returnedMailCard_small/Image07.webp"),
+        big: require('../../img/returnedMailCards/returnedMailCard/Image07.webp'),
+        back: require('../../img/returnedMailCards/returnedMailCard/Image06.webp'),
         unlocked: false,
         key: []
     },
--- psychological_grocery_shop_from_ycy/components/CardStore/personConfig.js
+++ psychological_grocery_shop_from_ycy/components/CardStore/personConfig.js
@@ -1,22 +1,22 @@
 export default [
     {
         id: 0,
-        small: require("../../img/O/personalCard_small/Image01.webp"),
-        big: require('../../img/O/personalCard/Image01.webp'),
+        small: require("../../img/personalCards/personalCard_small/Image01.webp"),
+        big: require('../../img/personalCards/personalCard/Image01.webp'),
         unlocked: true,
         key: []
     },
     {
         id: 1,
-        small: require("../../img/O/personalCard_small/Image02.webp"),
-        big: require('../../img/O/personalCard/Image05.webp'),
+        small: require("../../img/personalCards/personalCard_small/Image02.webp"),
+        big: require('../../img/personalCards/personalCard/Image05.webp'),
         unlocked: true,
         key: []
     },
     {
         id: 2,
-        small: require("../../img/O/personalCard_small/Image03.webp"),
-        big: require('../../img/O/personalCard/Image09.webp'),
+        small: require("../../img/personalCards/personalCard_small/Image03.webp"),
+        big: require('../../img/personalCards/personalCard/Image09.webp'),
         unlocked: false,
         key: []
     },
