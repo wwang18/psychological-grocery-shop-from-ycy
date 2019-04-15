321e8d4a3830bead566d758ad93aa3d5bb79478f
WeiWang88 <wwang18@kent.edu>
2019-04-14 20:34:29 -0500

    - change i page to gif

--- psychological_grocery_shop_from_ycy/screens/PageMailBox_i.js
+++ psychological_grocery_shop_from_ycy/screens/PageMailBox_i.js
@@ -43,7 +43,7 @@ class PageMailBox_i extends Component {
         <ImageBackground
           resizeMode="stretch" // ios下over会使图片部分显示不了
           style={styles.container}
-          source={require("./../img/i_page.webp")} //require("./../img/post_office.gif")
+          source={require("./../img/i_page.gif")} //require("./../img/post_office.gif")
         >
           <View style={{ flex: 1, flexDirection: "row" }}>
             <View style={{ flex: 184 }} />
